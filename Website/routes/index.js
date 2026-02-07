268ced24590fc7ec800404d83b179281967f7fc8
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-02-07 19:05:18 +0200

    fix: reorder routes for better organization and clarity

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AuthRouter } from "./auth/auth.js";
 import { PagesRouter } from "./pages/pages.js";
+import ProjectsRoutes from './projects/project.js'
+import workshopRoutes from "./workshop/workshop.js";
 
 const router = Router();
 
-router.use("/auth", AuthRouter);
 router.use("/", PagesRouter);
+router.use("/auth", AuthRouter);
+router.use("/projects" , ProjectsRoutes);
+router.use("/workshop", workshopRoutes);
 
 export { router as GlobalRouter };
