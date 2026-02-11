b62b2316038c3b979ef63af0747ff37f17dd1be2
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-02-12 00:23:35 +0200

    feat: add IPC handlers for workshop management and reporting

--- Business-Incubator-DB-System/Desktop/src/electron/main.js
+++ Business-Incubator-DB-System/Desktop/src/electron/main.js
@@ -2,6 +2,20 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 import { isDevelopmentMode } from './util.js';
 import { getPreloadPath, getUIPath } from './pathResolver.js';
 import loginRequest from './backend/auth/login.js';
+import {
+  getAllWorkshops,
+  getWorkshop,
+  createWorkshop,
+  updateWorkshop,
+  deleteWorkshop,
+  trackAttendance,
+  submitFeedback,
+  getWorkshopEnrollments,
+  getAttendanceReport,
+  getFeedbackReport,
+  exportAttendanceReportPDF,
+  exportFeedbackReportPDF,
+} from './backend/workshop/workshops.js';
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
@@ -19,6 +33,55 @@ app.on('ready', () => {
   }
   ipcMain.handle('auth:login', async (_event, credentials) => {
     return await loginRequest(credentials);
+  });
+
+  // Workshop IPC Handlers
+  ipcMain.handle('get-workshops', async () => {
+    return await getAllWorkshops();
+  });
+
+  ipcMain.handle('get-workshop', async (_event, id) => {
+    return await getWorkshop(id);
+  });
+
+  ipcMain.handle('create-workshop', async (_event, workshopData) => {
+    return await createWorkshop(workshopData);
+  });
+
+  ipcMain.handle('update-workshop', async (_event, id, workshopData) => {
+    return await updateWorkshop(id, workshopData);
+  });
+
+  ipcMain.handle('delete-workshop', async (_event, id) => {
+    return await deleteWorkshop(id);
+  });
+
+  ipcMain.handle('track-attendance', async (_event, enrollmentId, attended) => {
+    return await trackAttendance(enrollmentId, attended);
+  });
+
+  ipcMain.handle('submit-feedback', async (_event, enrollmentId, feedbackData) => {
+    return await submitFeedback(enrollmentId, feedbackData);
+  });
+
+  ipcMain.handle('get-workshop-enrollments', async (_event, workshopId) => {
+    return await getWorkshopEnrollments(workshopId);
+  });
+
+  ipcMain.handle('get-attendance-report', async () => {
+    return await getAttendanceReport();
+  });
+
+  ipcMain.handle('get-feedback-report', async () => {
+    return await getFeedbackReport();
+  });
+
+  ipcMain.handle('export-attendance-report', async () => {
+    return await exportAttendanceReportPDF();
+  });
+
+  ipcMain.handle('export-feedback-report', async () => {
+    return await exportFeedbackReportPDF();
   });
   handleCloseEvents(mainWindow);
 });
