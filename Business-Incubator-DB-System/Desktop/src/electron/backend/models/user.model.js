bda81427663e5e705e5383a35e4cbcd33a59058a
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-02-15 20:27:35 +0200

    fix(user): update database import from .js to .cjs

--- Business-Incubator-DB-System/Desktop/src/electron/backend/models/user.model.js
+++ Business-Incubator-DB-System/Desktop/src/electron/backend/models/user.model.js
@@ -1,8 +1,8 @@
-import pool from "../config/database.js";
+import pool from "../config/database.cjs";
 
 export const findUserByEmail = async (email) => {
-    const query = 'SELECT * FROM users WHERE email = $1';
-    const values = [email];
-    const result = await pool.query(query, values);
-    return result.rows[0];
+  const query = "SELECT * FROM users WHERE email = $1";
+  const values = [email];
+  const result = await pool.query(query, values);
+  return result.rows[0];
 };
