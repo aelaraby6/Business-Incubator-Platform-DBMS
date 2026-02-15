df9041ba570ade1c7e9ba818388e4f56ae57b2df
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-02-15 20:30:50 +0200

    fix(pages): add workshop detail route for improved workshop management

--- Website/routes/pages/pages.js
+++ Website/routes/pages/pages.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getAboutPage, getMentorsPage, getWorkshopsPage } from "../../controllers/pages/pages.controller.js";
+import {
+  getAboutPage,
+  getMentorsPage,
+  getWorkshopsPage,
+  getWorkshopDetailPage,
+} from "../../controllers/pages/pages.controller.js";
 
 const router = Router();
 
 router.get("/about", getAboutPage);
 router.get("/mentors", getMentorsPage);
 router.get("/workshop", getWorkshopsPage);
+router.get("/workshop/:id", getWorkshopDetailPage);
 
 export { router as PagesRouter };
